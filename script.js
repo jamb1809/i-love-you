d6c8449a8e74ea847c311c370f9d327f40c9889b
jamb1809 <angel70895@hotmail.com>
2024-02-13 18:13:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ $(document).ready(function(){
           // Hacer visible la carta con una animación ondulante
           $('#card').css({'visibility':'visible', 'opacity': 0, 'transform': 'scale(0.1)'});
           $('#card').animate({'opacity': 1}, {duration: 1000, step: function(now, fx) {
-            var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
-            $(this).css('transform', 'scale(' + scale + ')');
+            //var scale = 1 + Math.sin(now * Math.PI) * 0.1; // Calculamos la escala basada en la función seno
+            $(this).css('transform', 'scale(1.5)');
           }}); // Animación de ondulación
         });
       });
